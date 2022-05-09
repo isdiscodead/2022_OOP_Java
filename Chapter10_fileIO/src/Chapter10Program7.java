51ab67e1fe9ee13921bdf034791511ff335fa965
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-09 23:28:06 +0900

    main

--- Chapter10_fileIO/src/Chapter10Program7.java
+++ Chapter10_fileIO/src/Chapter10Program7.java
@@ -9,13 +9,14 @@ public class Chapter10Program7 {
 
 	static int NUM_OF_ACCOUNT = 20;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
 		File outFile = new File("./example4.data");
 		
 		FileOutputStream outFileStream = new FileOutputStream(outFile);
 		ObjectOutputStream outObjectStream = new ObjectOutputStream(outFileStream);
 		
+		// File로 만들고 읽고자 하는 객체는 Serializable 을 implements해야 함 ! 
 		Account[] accounts = new Account[NUM_OF_ACCOUNT];
 		
 		// create objects 
@@ -24,15 +25,26 @@ public class Chapter10Program7 {
 			accounts[i].setOwerName("Student"+(i+1));
 		}
 
+		// write 
 		outObjectStream.writeObject(accounts);
 		
 		outObjectStream.close();
 		System.out.println("complete!!");
 		
-		// read object -> 컴퓨터 구조를 좀 알아야 ?... 블로그 읽어보기 
+		// read object -> 역직렬화 
 		File inFile = new File("./example4.data");
 		FileInputStream inFileStream = new FileInputStream(inFile);
+		
+		// 읽어오기 
 		ObjectInputStream inObjectStream = new ObjectInputStream(inFileStream);
+		Object object = inObjectStream.readObject();
+		inObjectStream.close();
+		
+		// 형변환 필요 
+		Account[] inputAccounts = (Account[])object;
+		for ( int i=0 ; i<inputAccounts.length ; i++ ) {
+			System.out.println(inputAccounts[i].getOwnerName());
+		}
 	}
 
 }
